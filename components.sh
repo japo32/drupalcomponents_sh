bfd27836b65d9409c4a4303cec2e75aa721b53b1
Japo Domingo <japodomingo@gmail.com>
2013-11-29 14:17:53 +1100

    added opengraph and sharethis to module list.

--- components.sh
+++ components.sh
@@ -8,10 +8,10 @@ if [ -d "sites/all/modules" ]; then
     drush dis toolbar overlay
 
     #download some goodies. remove the -y to avoid overwrites
-    drush dl admin_menu ctools link date imce libraries mollom nice_menus nivo_slider pathauto sitemap token transliteration globalredirect page_title googleanalytics ckeditor jquery_update lightbox2 views webform xmlsitemap zen -y
+    drush dl admin_menu ctools link date imce libraries mollom nice_menus nivo_slider pathauto sitemap token transliteration globalredirect page_title googleanalytics ckeditor jquery_update lightbox2 views webform xmlsitemap opengraph_meta sharethis zen -y
 
     #enable the modules
-    drush en admin_menu admin_menu_toolbar ctools link date date_popup date_api imce libraries mollom nice_menus pathauto site_map token transliteration globalredirect page_title google_analytics ckeditor jquery_update lightbox2 views views_ui webform xmlsitemap xmlsitemap_menu zen -y
+    drush en admin_menu admin_menu_toolbar ctools link date date_popup date_api imce libraries mollom nice_menus pathauto site_map token transliteration globalredirect page_title google_analytics ckeditor jquery_update lightbox2 views views_ui webform xmlsitemap xmlsitemap_menu opengraph_meta sharethis zen -y
 
 
     echo "make sure to configure the libraries and folder settings"
